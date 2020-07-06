e0f5beeb696fec0cc51abce364afbe4024e350c0
iedwindavid <60800296+iedwindavid@users.noreply.github.com>
2020-07-06 18:30:28 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,70 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<link rel="stylesheet" type="text/css" href="css/newstyle.css">
+<script src="js/all.js"></script>
+<div class="contenedor-home">
+  <div class="home">
+    <h3>POLITECNICO INTERNACIONAL</h3>
+    <div class="contenedor-img-home">
+    <img src="images/Politecnicoint.jpg" width="20%" height="20%">
+    </div>
+    <p> Somos un grupo por empresarios que creen en Colombia, deseosos de retribuir a la sociedad y al país los
+      rendimientos económicos originados por sus actividades. El Politécnico Internacional es una Institución de
+      Educación Superior constituida como una fundación privada sin ánimo de lucro, la cual <strong>contribuye a la
+        formación integral de la juventud colombiana, para hacer realidad sus s  ueños a través de la empleabilidad o el
+        emprendimiento.</strong></p>
+    <p><strong>Es unatituc Insión comprometida con el desarrollo laboral</strong> de sus egresados y es reconocida por
+      las destacadas competencias prácticas y por los altos estándares de calidad y servicio que desarrolla en sus
+      estudiantes; los cuales tienen una gran acogida en el sector productivo.</p>
+  </div>
+  <div class="home">
+    <h3>Vision</h3>
+    <h4>Lo que nos diferencia</h4>
+    <p> <strong>Modelo decentralizado</strong>, la ubicación estratégica de nuestras cuatro sedes facilita a los jóvenes
+      poder ir a estudiar sin incurrir en mayores gastos, debido a la cercanía de estas a sus lugares de vivienda o
+      trabajo. Desde el 2019 implementamos nuestra modalidad virtual, dando más cobertura y llegando a más personas a
+      nivel nacional.</p>
+    <h4>Política de Calidad</h4>
+    <ol>
+      <li>La excelencia educativa a través de la pertinencia de los programas académicos.</li>
+      <li>Instalaciones impecables y pertinentes para satisfacer las necesidades del proceso formativo.</li>
+      <li>Docentes idóneos que se caracterizan porque saben de lo que enseñan y motivan el aprendizaje.</li>
+      <li>Procesos administrativos eficientes y eficaces para satisfacer a toda la comunidad</li>
+    </ol>
+  </div>
+  <div class="home">
+    <h4>Objetivos de Calidad</h4>
+    <ul>
+      <li>Ofrecer nuevos programas requeridos en mercado laboral</li>
+      <li>Crear la Unidad de Educación Virtual.</li>
+      <li>Fortalecer el desarrollo profesoral para garantizar la calidad de enseñanza.</li>
+      <li>Promover la cultura de investigación</li>
+      <li>Estimular el aprendizaje de una Segunda lengua</li>
+      <li>Ser una Institución de Carácter Tecnológico</li>
+      <li>Brindar procesos academico-administrativos eficases y eficientes</li>
+      <li>Proporcionar espacios físicos y tecnológicos idóneos para el aprendizaje</li>
+      <li>Incentivar las relaciones internacionales</li>
+    </ul>
+  </div>
+</div>
+<footer class="footer-1">
+<div class="footer-content">
+	<div class="footer-text1">
+    <h1>Sigános en redes Sociales y en nuestra página web<h1>
+    <div class="redes">
+    <a href="https://www.facebook.com/politecnicointernacionalcolombia"> <span class="tres"> <i class=" icon">
+    <i class="fab fa-facebook"></i> </span> </i> </a>
+    <a href="https://twitter.com/PolitecnicoIn?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"> <span class="tres"> <i class=" icon">
+    <i class="fab fa-twitter"></i> </span> </i> </a>
+    <a href="https://www.instagram.com/politecnicoin"> <span class="tres"> <i class=" icon">
+    <i class="fab fa-instagram"></i> </span> </i> </a>
+  </div> </div>
+  <p class="copy">&copy: Grupo Mecánica dental - 2020 // Todos los derechos reservados</p>
+
+
+	<div class="logo">
+  <a  href="https://politecnicointernacional.edu.co/sw/es/home"> <img src="https://politecnicointernacional.edu.co/sw/sites/default/files/glazed-cms-media/logo-polinternacional-vrtl_0.png" alt="Politécnico internacional" title="" style="margin-top: 7px; width: 60%;"> </a>
+
+    
+        </div>
+</div>
